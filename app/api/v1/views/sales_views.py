0e810f0b656099b9ab8c564fcf32a2e70739d01a
Deekerubo <dianakerubo24@gmail.com>
2018-10-18 16:47:14 +0300

    :exclamation:[Feature #161204844] Created amethod to POST all the sale order

--- app/api/v1/views/sales_views.py
+++ app/api/v1/views/sales_views.py
@@ -8,3 +8,19 @@ from app.api.v1.models.sales_models import Order
 
 
 cart = []
+
+class NewOrder(Resource):
+    def __init__(self):
+        self.orders = Order()
+
+    '''Create a  sale  order'''
+    def post(self):
+        data = request.get_json()
+        item_name = data['name']
+        item_price = data['price']
+        item_description = data['description']
+        item_quantity = data['quantity']
+        item_category = data['category']
+
+        new_order = self.orders.add_order(item_name, item_price, item_description, item_quantity, item_category)
+        return make_response(jsonify({'Cart_Items': new_order}), 201)
