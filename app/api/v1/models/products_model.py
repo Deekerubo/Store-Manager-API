d6f1260e424c22303e23b4777a4a4e07787b8b73
Deekerubo <dianakerubo24@gmail.com>
2018-10-18 15:48:08 +0300

    :exclamation:[Feature #161314158] Define a function to add an entry into the models.py

---
+++ app/api/v1/models/products_model.py
@@ -0,0 +1,23 @@
+"""app/v1/models.py contains models for the app"""
+Entries_list = []
+
+class Entry(object):
+    """Add new entry"""
+    # constructor
+    def __init__(self):
+        # all entries placeholder
+        self.entries = Entries_list
+
+    def add_entry(self, name, quantity, description, price, category):
+        """Adds new entries"""
+
+        single_entry_holder = { "item_id" : len(self.entries)+1,
+                                    "item_name": name,
+                                    "item_description": description,
+                                    "item_quantity":quantity,
+                                    "item_price": price,
+                                    "item_category": category
+                               }
+        self.entries.append(single_entry_holder)
+            
+        return single_entry_holder
