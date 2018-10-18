5ffac7f465c12f34604204112372fa7a5d290bd6
Deekerubo <dianakerubo24@gmail.com>
2018-10-18 15:52:47 +0300

    :exclamation:[Feature #161314158] defines a function that creates the sales orders and initialize it with order class

---
+++ app/api/v1/models/sales_model.py
@@ -0,0 +1,25 @@
+'''contains models for the app'''
+Order_List = []
+
+class Order(object):
+     """Add new entry"""
+     # constructor
+     def __init__(self):
+        # all orders placeholder
+        self.orders =Order_List
+
+     def add_order(self, name, description, price, quantity, category):
+        """Adds new orders"""
+
+        if name and description  and price and quantity and category:
+           
+                single_order_holder = {"item_id" : len(self.orders)+1,
+                                       "item_name": name,
+                                       "item_description": description,
+                                       "item_quantity":quantity,
+                                       "item_price": price,
+                                       "item_category": category
+                                    }
+                self.orders.append(single_order_holder)
+                
+                return(single_order_holder)
