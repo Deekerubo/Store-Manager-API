ecee55ac563f669c0ac30e618e559594103057c0
Deekerubo <dianakerubo24@gmail.com>
2018-10-26 11:10:10 +0300

    [Chore #161500877] Update my Heroku Page

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from .views.product_views import Product, SingleProduct
 from .views.sales_views import NewOrder, SingleOrder
 from .views.users_views import UserRegistration, UserLogin
+from .views.home import Home
 
 version1 = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(version1)
@@ -15,3 +16,4 @@ api.add_resource(NewOrder, '/sales')
 api.add_resource(SingleOrder, '/sales/<int:salesID>')
 api.add_resource(UserRegistration, '/signup')
 api.add_resource(UserLogin, '/login')
+api.add_resource(Home, '/')
