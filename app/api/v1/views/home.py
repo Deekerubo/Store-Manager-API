4bf586ab1f5bd2a30b9b382df3b7a2cc40cc1314
Deekerubo <dianakerubo24@gmail.com>
2018-10-26 11:19:30 +0300

    [Chore #161500877] Update my Heroku Page

--- app/api/v1/views/home.py
+++ app/api/v1/views/home.py
@@ -1,14 +1,14 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, redirect
 from flask_restful import Resource
 from flask_restful import Api
 from flask import Blueprint
 
 class Home(Resource):
     def get(self):
-        return return redirect("https://documenter.getpostman.com/view/4775740/RWgxvvBe", code=302)
+        return redirect("https://documenter.getpostman.com/view/4775740/RWgxvvBe", code=302)
 
 home = Blueprint ('home',__name__)
 
 api = Api(home)
 
-api.add_resource(Home, '/')
+# api.add_resource(Home, '/home')
