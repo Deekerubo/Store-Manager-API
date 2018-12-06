29a59bd431ca150b7fec2de83ccd938870e824c8
Deekerubo <dianakerubo24@gmail.com>
2018-12-06 14:41:57 +0300

    [bug 162129380]Add tests

--- app/tests/v2/test_user.py
+++ app/tests/v2/test_user.py
@@ -15,7 +15,7 @@ class Test_User_Case(UserAuth):
         destroy_tables()
         create_tables()
         self.signup_data = { "email": "kerubo2@gmail.com", "username":"Diana", "password":"1kerubo", "role":True}
-        self.register_user1 = { "email": "diana1@gmail.com", "password":"1kerubo", "role":True}
+        self.register_user1 = { "email": "kerubo2@gmail.com", "username":"Diana", "password":"1kerubo", "role":True}
         self.register_user_empty_email = { "email": "", "username":"Diana", "password":"1kerubo", "role":True}
         self.register_user_invalid_email = { "email": "test.gmailcom", "username":"Diana", "password":"1kerubo", "role":True}
         self.register_user_empty_password = { "email": "test@gmail.com", "username":"Diana", "password":"", "role":True}
@@ -34,10 +34,26 @@ class Test_User_Case(UserAuth):
         data = json.loads(res.data.decode())
         self.assertEqual(data['message'], 'Store attendant was created succesfully')
         self.assertEqual(data["status"], "ok")
-        self.assertEqual(res.status_code, 201)   
+        self.assertEqual(res.status_code, 201) 
+
+    # def test_signup_twice(self):
+    #     '''Tests signup twice with the same email Address'''
+    #     self.app.post(USERSIGNUP_URL, 
+    #                            data=json.dumps(self.signup_data),
+    #                            content_type = 'application/json')
+    #     res = self.app.post(USERSIGNUP_URL,
+    #                            data=json.dumps(self.register_user1,
+    #                            content_type='application/json')
+    #     data = json.loads(res.data.decode())
+    #     self.assertEqual(data['message'], 'email already exist')
+    #     self.assertEqual(res.status_code, 400)
+
 
     def test_sign_up_empty_email(self):
         '''Test signup with an emptry email address'''
+        self.app.post(USERSIGNUP_URL, 
+                               data=json.dumps(self.signup_data),
+                               content_type = 'application/json')
         res = self.app.post(USERSIGNUP_URL,
                                data=json.dumps(self.register_user_empty_email),
                                content_type='application/json')
@@ -47,6 +63,9 @@ class Test_User_Case(UserAuth):
 
     def test_sign_up_invalid_email(self):
         '''Test signup with an invalid email address'''
+        self.app.post(USERSIGNUP_URL, 
+                               data=json.dumps(self.signup_data),
+                               content_type = 'application/json')
         res = self.app.post(USERSIGNUP_URL,
                                data=json.dumps(self.register_user_invalid_email),
                                content_type='application/json')
@@ -56,6 +75,9 @@ class Test_User_Case(UserAuth):
 
     def test_sign_up_empty_password(self):
         '''Test signup with an empty password'''
+        self.app.post(USERSIGNUP_URL, 
+                               data=json.dumps(self.signup_data),
+                               content_type = 'application/json')
         res = self.app.post(USERSIGNUP_URL,
                                data=json.dumps(self.register_user_empty_password),
                                content_type='application/json')
@@ -65,12 +87,16 @@ class Test_User_Case(UserAuth):
 
     def test_sign_up_short_password(self):
         '''Test signup with a short password'''
+        self.app.post(USERSIGNUP_URL, 
+                               data=json.dumps(self.signup_data),
+                               content_type = 'application/json')
         res = self.app.post(USERSIGNUP_URL,
                                data=json.dumps(self.register_user_short_password),
                                content_type='application/json')
         data = json.loads(res.data.decode())
         self.assertEqual(data['message'], 'Password should be atleast 6 characters')
         self.assertEqual(res.status_code, 400)
+    #Test LogIn
 
     def test_user_login(self):
         '''Test login success'''
